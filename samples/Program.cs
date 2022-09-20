fb4a85b22b9f1ba1238264e72e01acbf285500fd
Mattias Karlsson <matkar@wcom.se>
2022-09-20 11:59:17 +0200

    Update Example to .NET 6

--- samples/Program.cs
+++ samples/Program.cs
@@ -1,27 +1,19 @@
-﻿using System;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.DependencyInjection;
 using Spectre.Console.Cli;
 using Spectre.Cli.Extensions.DependencyInjection;
 
-namespace samples
+var services = new ServiceCollection();
+services.AddSingleton<GreetingService>();
+
+// add extra services to the container here
+using var registrar = new DependencyInjectionRegistrar(services);
+var app = new CommandApp(registrar);
+app.SetDefaultCommand<InfoCommand>();
+app.Configure(config =>
 {
-    class Program
-    {
-        static int Main(string[] args)
-        {
-            var services = new ServiceCollection();
-            services.AddSingleton<GreetingService>();
-            // add extra services to the container here
-            using var registrar = new DependencyInjectionRegistrar(services);
-            var app = new CommandApp(registrar);
-            app.SetDefaultCommand<InfoCommand>();
-            app.Configure(config =>
-            {
-                config.AddCommand<InfoCommand>("greet");
-                // configure your commands as per usual
-                // commands are automatically added to the container
-            });
-            return app.Run(args);
-        }
-    }
-}
+    config.AddCommand<InfoCommand>("greet");
+    // configure your commands as per usual
+    // commands are automatically added to the container
+});
+
+return app.Run(args);
